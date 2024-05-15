44d3f96fabe3c2a7a25cb0ffe44a3dfc850aa321
Matt <160069337+MatthieuLa@users.noreply.github.com>
2024-05-15 13:10:29 +0200

    Ajout du code de la session précèdente, et de l'url pour le fetch uploadWork.

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,7 +1,6 @@
 // --------------------- Variables --------------------- //
 
 let works = [];
-let modal = null;
 let uploadWorkInitialized = false; // Variable pour savoir si la fonction uploadWork a déjà été initialisée.
 
 // ---------------------  DOM Listener --------------------- //
@@ -69,15 +68,9 @@ fetch("http://localhost:5678/api/works")
     });
   });
 
-// --------------------- Event Listeners --------------------- //
-
-addEventListener("click", backToGallery);
-addEventListener("click", btnModal);
-addEventListener("click", closeModal);
-addEventListener("click", uploadWork);
-
 // --------------------- QuerySelectors --------------------- //
 
+const uploadForm = document.querySelector(".upload-form");
 const btnUpload = document.querySelector(".btn-upload");
 const fileInput = document.querySelector("#file-input");
 const uploadIcon = document.querySelector(".upload-icon");
@@ -87,14 +80,80 @@ const btnValidate = document.querySelector(".btn-modal__validate");
 const modalGallery = document.querySelector("#modal-gallery");
 const modalUpload = document.querySelector("#modal-upload");
 const closeButtons = document.querySelectorAll(".btn-close");
-const titleInput = document.querySelector('input[type="text"]');
+const titleInput = document.querySelector("#title");
+const categorySelect = document.querySelector("#category");
+
+// --------------------- Event Listeners --------------------- //
+
+addEventListener("click", backToGallery);
+addEventListener("click", btnModal);
+
+btnUpload.addEventListener("click", () => {
+  fileInput.click();
+});
+
+closeButtons.forEach((button) => {
+  button.addEventListener("click", closeModal);
+});
+
+// Le formulaire doit inclure les différent inputs, et l'évènement pour l'envoi des données doit se faire lors de la soumission du formulaire.#category
+uploadForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  uploadIcon.style.display = "none";
+  btnUpload.style.display = "none";
+  uploadRestriction.style.display = "none";
+
+  // Créer un FormData en utilisant le formulaire. Les valeurs seront pré-remplies en fonction du name (key) et de la valeur des inputs (value).
+  const formData = new FormData(uploadForm);
+
+  if (
+    !formData.get("title").trim() ||
+    !formData.get("category") ||
+    !formData.get("image")
+  ) {
+    alert("Une information est manquante au sein du formulaire.");
+    // @TODO : Si le titre est vide, afficher une erreur.
+  } else {
+    // @TODO : Envoyer les données à l'API, et recevoir le nouveau "work" en réponse.
+    uploadWork(formData);
+  }
+});
+
+function uploadWork(form) {
+  const token = localStorage.getItem("token");
+  fetch("http://localhost:5678/api/works", {
+    // a remplire avec le bon fetch.
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: form,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // @TODO : Ajouter le nouveau work aux galleries. Et la preview d'images.
+      addUploadedWorkToGallery(data);
+    });
+}
+
+function addUploadedWorkToGallery(work) {
+  const gallery = document.querySelector(".gallery");
+  const figureGallery = document.createElement("figure");
+  btnUpload.addEventListener("click", () => {
+    figureGallery.innerHTML = `
+        <img src="${work.imageUrl}" alt="${work.title}" />
+        <figcaption>${work.title}</figcaption>
+        `;
+    gallery.appendChild(figureGallery);
+  });
+}
 
 // --------------------- Fonctions --------------------- //
 
 // Fonction qui ajoute les catégories de travaux dynamiquement en fonction de l'api.
 
 function modalCategory() {
-  const modalCategory = document.querySelector("#modal-category");
+  const modalCategory = document.querySelector("#category");
   // Si la modal contient déjà des enfants, on ne fait rien (permet d'éviter de dupliquer les catégories à chaque ouverture de la modal)
   if (modalCategory.children.length > 0) return;
   modalCategory.innerHTML = "";
@@ -111,8 +170,31 @@ function modalCategory() {
     });
 }
 
-// Fonction pour uploader un travail [WIP].
+// @TODO : Gérer la preview de l'image.
 
+const inputs = [fileInput, titleInput, categorySelect];
+for (let input of inputs) {
+  input.addEventListener("input", validateInputs);
+}
+
+function validateInputs() {
+  console.log(!!titleInput.value.trim());
+  console.log(!!Number(categorySelect.value));
+  console.log(!!fileInput.files[0]);
+  if (
+    titleInput.value.trim() &&
+    Number(categorySelect.value) &&
+    fileInput.files[0]
+  ) {
+    btnValidate.style.backgroundColor = "#1D6154";
+    btnValidate.disbaled = false;
+  } else {
+    btnValidate.style.backgroundColor = "#A7A7A7";
+    btnValidate.disbaled = true;
+  }
+}
+
+/*
 function uploadWork() {
   // Si uploadWorkInitialized est faux, on initialise les évènements.
   if (!uploadWorkInitialized) {
@@ -180,20 +262,17 @@ function validateUpload(event) {
   btnUpload.style.display = "block";
   uploadRestriction.style.display = "block";
 }
+*/
 
 // Boutons de fermeture des modales
 
 function closeModal() {
-  closeButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      modalGallery.style.display = "none";
-      modalGallery.setAttribute("aria-hidden", true);
-      modalGallery.removeAttribute("aria-modal");
-      modalUpload.style.display = "none";
-      modalUpload.setAttribute("aria-hidden", true);
-      modalUpload.removeAttribute("aria-modal");
-    });
-  });
+  modalGallery.style.display = "none";
+  modalGallery.setAttribute("aria-hidden", true);
+  modalGallery.removeAttribute("aria-modal");
+  modalUpload.style.display = "none";
+  modalUpload.setAttribute("aria-hidden", true);
+  modalUpload.removeAttribute("aria-modal");
 }
 
 // Fonction pour afficher la modal 2 et cacher la modal 1
